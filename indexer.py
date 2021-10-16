9419ecb097f60884355f5555205b25e7ff1ba20a
Anusha Narthu <78515787+AnushaNarthu@users.noreply.github.com>
2021-10-16 01:27:36 -0400

    Update indexer.py

--- indexer.py
+++ indexer.py
@@ -53,7 +53,13 @@ class Indexer:
     def add_skip_connections(self):
         """ For each postings list in the index, add skip pointers.
             To be implemented."""
-        raise NotImplementedError
+        for value in self.inverted_index.values():
+            value.add_skip_connections()
+
+        """ For each postings list in the index, add skip pointers.
+            To be implemented."""
+            
+        return
 
     def calculate_tf_idf(self):
         """ Calculate tf-idf score for each document in the postings lists of the index.
