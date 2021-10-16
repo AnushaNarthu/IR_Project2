0adda543c4db891e93bd5f970922a3eb14d47583
Anusha Narthu <78515787+AnushaNarthu@users.noreply.github.com>
2021-10-16 02:41:33 -0400

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -8,7 +8,7 @@ import math
 
 class Node:
 
-    def __init__(self, value=None, next=None):
+    def __init__(self, value=None, next=None, skipper = None):
         """ Class to define the structure of each node in a linked list (postings list).
             Value: document id, Next: Pointer to the next node
             Add more parameters if needed.
